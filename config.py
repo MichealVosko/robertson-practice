5958e67036d47104da91148e980ded7d96b6b699
Im-Moazzam <moazzamaleem788@gmail.com>
2025-10-14 20:03:11 +0500

    refactor: switch from OpenAI to Google Generative AI and update dependencies

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
--- models/llm.py
+++ models/llm.py
@@ -1,13 +1,13 @@
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from pydantic import BaseModel, Field
 from typing import List, Annotated
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
-llm = ChatOpenAI(
-    model="gpt-4o-mini",
-    openai_api_key = os.getenv("OPENAI_API_KEY")
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=os.getenv("GOOGLE_API_KEY"),
 )
 
 # Structured output for CPT
